06cefb1b177931d342ade84460ae1d75c43fb70b
Hendrik Van Belleghem <hendrik.vanbelleghem@gmail.com>
2016-11-25 23:25:49 +0100

    Updates for create and update code. Integration with SQLite

--- lib/Net/Cisco/ACS/Mock/Controller/User.pm
+++ lib/Net/Cisco/ACS/Mock/Controller/User.pm
@@ -1,6 +1,7 @@
 package Net::Cisco::ACS::Mock::Controller::User;
 use Mojo::Base 'Mojolicious::Controller';
 use Data::Dumper;
+use XML::Simple;
 
 my %users =
 ( "foo" =>
@@ -52,22 +53,79 @@ sub query {
 	{ my $query_rs = $rs->search({ id => $id });
       $user = $query_rs->first;
     }
-	$self->stash("user" => $user);
-	$self->render(template => 'user/query', format => 'xml');
+    if (!$id && !$name)
+    { my $query_rs = $rs->search;
+      my %users = ();
+      while (my $account = $query_rs->next)
+      { $users{$account->name} =
+        { id => $account->id, name => $account->name, description => $account->description,
+          identitygroupname => $account->identitygroupname, changepassword => $account->changepassword,
+          enablepassword => $account->enablepassword, enabled => $account->enabled, password => $account->password,
+          passwordneverexpires => $account->passwordneverexpires, passwordtype => $account->passwordtype,
+          dateexceeds => $account->dateexceeds, dateexceedsenabled =>$account->dateexceedsenabled
+        };
+      }
+
+      $self->stash("users" => \%users);
+      $self->render(template => 'user/queryall', format => 'xml', layout => 'userall');
+      return;
+    }
+    
+    $self->stash("user" => $user);
+	$self->render(template => 'user/query', format => 'xml', layout => 'user');
 }
 
 sub update {
     my $self = shift;
-	my $name = $self->param("name");
-	my $id = $self->param("id");
-	$self->render(template => 'user/update', format => 'xml');	
+    my $rs = $self->db->resultset('User');
+    my $data = $self->req->body;
+    my $xmlsimple = XML::Simple->new();
+    my $xmlout = $xmlsimple->XMLin($data);
+    my $query_rs = $rs->search({ name => $xmlout->{"name"} });
+    my $account = $query_rs->first;
+    $xmlout->{"dateExceedsEnabled"} = $xmlout->{"dateExceedsEnabled"} eq "true" ? 1 : 0;
+    $xmlout->{"enabled"} = $xmlout->{"enabled"} eq "true" ? 1 : 0;
+    $xmlout->{"enablePassword"} = $xmlout->{"enablePassword"} eq "true" ? 1 : 0;
+    $xmlout->{"passwordNeverExpires"} = $xmlout->{"passwordNeverExpires"} eq "true" ? 1 : 0;    
+    $account->update({ description => $xmlout->{"description"},
+          identitygroupname => $xmlout->{"identityGroupName"},
+          changepassword => $xmlout->{"changePassword"},
+          enablepassword => $xmlout->{"enablePassword"},
+          enabled => $xmlout->{"enabled"},
+          password => $xmlout->{"password"},
+          passwordneverexpires => $xmlout->{"passwordNeverExpires"},
+          passwordtype => $xmlout->{"passwordType"},
+          dateexceeds => $xmlout->{"dateExceeds"},
+          dateexceedsenabled =>$xmlout->{"dateExceedsEnabled"}
+          });
+	$self->render(render => '', status => 200);	
 }
 
 sub create {
     my $self = shift;
-	my $name = $self->param("name");
-	my $id = $self->param("id");
-	$self->render(template => 'user/create', format => 'xml');	
+    my $data = $self->req->body;
+    my $xmlsimple = XML::Simple->new();
+    my $xmlout = $xmlsimple->XMLin($data);
+    $xmlout->{"dateExceedsEnabled"} = $xmlout->{"dateExceedsEnabled"} eq "true" ? 1 : 0;
+    $xmlout->{"enabled"} = $xmlout->{"enabled"} eq "true" ? 1 : 0;
+    $xmlout->{"enablePassword"} = $xmlout->{"enablePassword"} eq "true" ? 1 : 0;
+    $xmlout->{"passwordNeverExpires"} = $xmlout->{"passwordNeverExpires"} eq "true" ? 1 : 0;    
+    $xmlout->{"dateExceeds"} = ref($xmlout->{"dateExceeds"}) ? "" : $xmlout->{"dateExceeds"};
+    $self->db->resultset('User')->create({
+          name => $xmlout->{"name"},
+          description => $xmlout->{"description"},
+          identitygroupname => $xmlout->{"identityGroupName"},
+          changepassword => $xmlout->{"changePassword"},
+          enablepassword => $xmlout->{"enablePassword"}, # HASH ?!?
+          enabled => $xmlout->{"enabled"},
+          password => $xmlout->{"password"},
+          passwordneverexpires => $xmlout->{"passwordNeverExpires"},
+          passwordtype => $xmlout->{"passwordType"},
+          dateexceeds => $xmlout->{"dateExceeds"}, # HASH?!?
+          dateexceedsenabled =>$xmlout->{"dateExceedsEnabled"},
+          id => "9999"
+          });
+	$self->render(render => '', status => 200);	
 }
 
 sub delete {
